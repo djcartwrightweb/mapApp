be290de545ebd6c157883aa47032a15a28c888fe
DJ Cartwright <dj.cartwright.web@gmail.com>
2025-02-03 10:36:54 -0500

    Initial Commit

---
+++ MapApp/MapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapAppApp.swift
+//  MapApp
+//
+//  Created by David Cartwright on 2025-02-03.
+//
+
+import SwiftUI
+
+@main
+struct MapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
