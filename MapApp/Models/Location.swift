efb501ed8dfcbf4514ec93f68b485feae7858a3d
DJ Cartwright <dj.cartwright.web@gmail.com>
2025-02-03 11:27:21 -0500

    complete lesson 1

---
+++ MapApp/Models/Location.swift
@@ -0,0 +1,18 @@
+//
+//  Location.swift
+//  MapApp
+//
+//  Created by David Cartwright on 2025-02-03.
+//
+
+import Foundation
+import MapKit
+
+struct Location {
+    let name: String
+    let cityName: String
+    let coordinates: CLLocationCoordinate2D
+    let description: String
+    let imageNames: [String]
+    let link: String
+}
